6171c590099d7ace44066c563522e2e186c7444b
Kevin Day <kday@library.tamu.edu>
2018-10-01 09:12:24 -0500

    Improve model testings

--- src/main/webapp/tests/unit/models/abstractAppModelTest.js
+++ src/main/webapp/tests/unit/models/abstractAppModelTest.js
@@ -1,8 +1,30 @@
 describe('model: AbstractAppModel', function () {
+  var rootScope, scope, WsApi, AbstractAppModel;
 
-    beforeEach(function() {
-        module('core');
-        module('sage');
+  beforeEach(function() {
+    module('core');
+    module('sage');
+    module('mock.wsApi');
+
+    inject(function ($rootScope, _WsApi_, HttpMethodVerbs, _AbstractAppModel_) {
+      rootScope = $rootScope;
+      scope = $rootScope.$new();
+      WsApi = _WsApi_;
+
+      AbstractAppModel = _AbstractAppModel_;
     });
+  });
 
+  describe('Is the model defined', function () {
+    it('should be defined', function () {
+      expect(AbstractAppModel).toBeDefined();
+    });
+  });
+
+  describe('Initialization should return AbstractAppModel', function() {
+    it('the AbstractAppModel was returned', function() {
+      var model = new AbstractAppModel();
+      expect(typeof model).toEqual("object");
+    });
+  });
 });
--- src/main/webapp/tests/unit/models/jobModelTest.js
+++ src/main/webapp/tests/unit/models/jobModelTest.js
@@ -1,8 +1,31 @@
-describe('model: JobModel', function () {
+describe('model: Job', function () {
+  var rootScope, scope, WsApi, Job;
 
-    beforeEach(function() {
-        module('core');
-        module('sage');
+  beforeEach(function() {
+    module('core');
+    module('sage');
+    module('mock.wsApi');
+
+    inject(function ($rootScope, _WsApi_, HttpMethodVerbs, _Job_) {
+      rootScope = $rootScope;
+      scope = $rootScope.$new();
+      WsApi = _WsApi_;
+
+      Job = _Job_;
     });
+  });
+
+  describe('Is the model defined', function () {
+    it('should be defined', function () {
+      expect(Job).toBeDefined();
+    });
+  });
+
+  describe('Initialization should return Job', function() {
+    it('the Job was returned', function() {
+      var model = new Job();
+      expect(typeof model).toEqual("object");
+    });
+  });
 
 });
