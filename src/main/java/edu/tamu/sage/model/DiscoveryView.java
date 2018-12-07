d6b30acd49778a76b52bf4e170befa92b4a899a0
jeremythuff <jeremythuff@gmail.com>
2018-12-07 09:48:47 -0600

    WIP added some properties to Discovery View

--- src/main/java/edu/tamu/sage/model/DiscoveryView.java
+++ src/main/java/edu/tamu/sage/model/DiscoveryView.java
@@ -2,14 +2,25 @@ package edu.tamu.sage.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import edu.tamu.sage.model.validation.DiscoveryViewValidator;
 import edu.tamu.weaver.validation.model.ValidatingBaseEntity;
 
 @Entity
 public class DiscoveryView extends ValidatingBaseEntity {
+    
     @Column(unique=true)
     private String name;
+    
+    @ManyToOne
+    private Source source;
+    
+    @Column
+    private String filter;
+    
+    @Column
+    private String slug;
 
     public DiscoveryView() {
         setModelValidator(new DiscoveryViewValidator());
@@ -22,5 +33,31 @@ public class DiscoveryView extends ValidatingBaseEntity {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
+
+    public String getFilter() {
+        return filter;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+    
+    
     
 }
