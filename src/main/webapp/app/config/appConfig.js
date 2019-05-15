3d498cf52c6c21e225ec921d772d61fcd203d01b
jeremythuff <jeremythuff@gmail.com>
2019-05-15 14:20:50 -0500

    Reverted configuration

--- src/main/webapp/app/config/appConfig.js
+++ src/main/webapp/app/config/appConfig.js
@@ -1,44 +1,44 @@
 var appConfig = {
 
-    'version': '1.0.x',
+  'version': '1.0.x',
 
-    'allowAnonymous': true,
-    'anonymousRole': 'ROLE_ANONYMOUS',
+  'allowAnonymous': true,
+  'anonymousRole': 'ROLE_ANONYMOUS',
 
-    'authStrategies': ['emailRegistration'],
+  'authStrategies': ['emailRegistration'],
 
-    'authService': window.location.protocol + '//' + window.location.host + window.location.base + '/auth',
-    'webService': window.location.protocol + '//' + window.location.host + window.location.base,
+  'authService': window.location.protocol + '//' + window.location.host + window.location.base + '/auth',
+  'webService': window.location.protocol + '//' + window.location.host + window.location.base,
 
-    'storageType': 'session',
+  'storageType': 'session',
 
-    'logging': {
-        'log': true,
-        'info': true,
-        'warn': true,
-        'error': true,
-        'debug': true
-    },
+  'logging': {
+      'log': true,
+      'info': true,
+      'warn': true,
+      'error': true,
+      'debug': true
+  },
 
-    'stompDebug': false,
+  'stompDebug': false,
 
-    /*
-      Determines the type of connection stomp will attempt to make with the service.
-      TYPES:  websocket, xhr-streaming, xdr-streaming, eventsource, iframe-eventsource,
-          htmlfile, iframe-htmlfile, xhr-polling, xdr-polling, iframe-xhr-polling,
-          jsonp-polling
-    */
-    'sockJsConnectionType': ['websocket', 'xhr-polling'],
+  /*
+    Determines the type of connection stomp will attempt to make with the service.
+    TYPES:  websocket, xhr-streaming, xdr-streaming, eventsource, iframe-eventsource,
+        htmlfile, iframe-htmlfile, xhr-polling, xdr-polling, iframe-xhr-polling,
+        jsonp-polling
+  */
+  'sockJsConnectionType': ['websocket', 'xhr-polling'],
 
-    // Set this to 'admin' or 'user' if using mock AuthService
-    // otherwise set to null or false
-    'mockRole': null,
+  // Set this to 'admin' or 'user' if using mock AuthService
+  // otherwise set to null or false
+  'mockRole': null,
 
 
-    'contentMap': {"image": ["image/jpeg","image/png","image/gif", "text/html;charset=UTF-8", "jpg"],"seadragon": ["image/jp2","image/tiff"]},
+  'contentMap': {"image": ["image/jpeg","image/png","image/gif", "text/html;charset=UTF-8", "jpg"],"seadragon": ["image/jp2","image/tiff"]},
 
-    'cantaloupeBaseUrl': 'https://api-dev.library.tamu.edu/iiif/2/',
+  'cantaloupeBaseUrl': 'https://api-dev.library.tamu.edu/iiif/2/',
 
-    'defaultThumbnailURI': "resources/images/default-thumbnail.jpg"
+  'defaultThumbnailURI': "resources/images/default-thumbnail.jpg"
 
 };
