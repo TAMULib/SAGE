3d90ceee04d3d616a84ce7112d26a2b4601b2978
William Welling <wwelling@library.tamu.edu>
2020-03-24 12:51:03 -0500

    mirador viewer

--- src/main/webapp/app/directives/contentViewerDirective.js
+++ src/main/webapp/app/directives/contentViewerDirective.js
@@ -1,27 +1,61 @@
-sage.directive("contentviewer", function() {
+sage.directive("contentviewer", function($filter, $timeout) {
   var viewerMap = appConfig.contentMap;
   return {
     templateUrl: "views/directives/viewers/viewerWrapper.html",
     scope: {
       contentType: "=",
-      resource: "="
+      context: "="
     },
     link: function($scope) {
-      var viewerTemplate = "default";
-      typeLoop:
-      for (var type in viewerMap) {
-        for (var supportedType in viewerMap[type]) {
-          if ($scope.contentType === viewerMap[type][supportedType]) {
-            viewerTemplate = type;
-            break typeLoop;
+      var viewerTemplate = 'default';
+
+      if ($scope.contentType === 'manifest') {
+        viewerTemplate = 'mirador';
+        $scope.resource = $filter("removeBrackets")($scope.context.manifestUri);
+      } else {
+        typeLoop:
+        for (var type in viewerMap) {
+          for (var supportedType in viewerMap[type]) {
+            if ($scope.contentType === viewerMap[type][supportedType]) {
+              viewerTemplate = type;
+              break typeLoop;
+            }
           }
         }
+        $scope.resource = $filter("removeBrackets")($scope.context.resourceLocationUri);
       }
+
       if (viewerTemplate === 'seadragon') {
         $scope.options = {};
         $scope.options.prefixUrl = 'resources/images/';
         $scope.options.tileSources = [$scope.resource];
       }
+     
+      if (viewerTemplate === 'mirador') {
+        $scope.loadViewer = function () {
+          Mirador({
+            id: 'mirador-wrapper',
+            data: [{ 
+              manifestUri: $scope.resource,
+              location: "TAMU"
+            }],
+            mainMenuSettings: {
+              show: false
+            },
+            windowObjects: [{
+              loadedManifest: $scope.resource,
+              viewType: 'ImageView',
+              displayLayout: false,
+              bottomPanel: false,
+              bottomPanelAvailable: false,
+              bottomPanelVisible: false,
+              sidePanel: false,
+              annotationLayer: false
+            }]
+          });
+        };
+      }
+
       $scope.includeTemplateUrl = "views/directives/viewers/"+viewerTemplate+"Viewer.html";
     },
     restrict: "E",
