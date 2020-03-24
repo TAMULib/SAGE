b2028dae8904dfaaed62e8ed635db9688728ca9d
William Welling <wwelling@library.tamu.edu>
2020-03-24 11:14:39 -0500

    simple manifest service with cache, thumbnail url lookup

---
+++ src/main/webapp/app/services/manifestService.js
@@ -0,0 +1,40 @@
+sage.factory("ManifestService", function($http, $q) {
+  var manifestService = this;
+
+  var cache = {};
+
+  manifestService.getManifest = function (url, ignoreCache) {
+    return $q(function(resolve, reject) {
+      if (!ignoreCache && cache[url]) {
+        resolve(cache[url]);
+      } else {
+        $http({
+          method: "GET",
+          url: url
+        }).then(function success(response) {
+          cache[url] = response.data;
+          resolve(cache[url]);
+        }, function error(error) {
+          console.error(error);
+          reject(error);
+        });
+      }
+    });
+  };
+
+  manifestService.getThumbnailUrl = function(url, ignoreCache) {
+    return $q(function(resolve, reject) {
+      manifestService.getManifest(url, ignoreCache).then(function(manifest) {
+        if (manifest && manifest.thumbnail) {
+          resolve(manifest.thumbnail['@id']);
+        } else {
+          reject('No manifest thumbnail found!');
+        }
+      }, function() {
+        reject(error);
+      })
+    });
+  }
+
+  return manifestService;
+});
