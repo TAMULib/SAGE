de3895968d13768acee90b6ef4b0f8d8f9cb6f15
Jason Savell <jsavell@library.tamu.edu>
2020-07-06 15:26:16 -0500

    instantiate RegExp

--- src/main/webapp/app/directives/presentValueDirective.js
+++ src/main/webapp/app/directives/presentValueDirective.js
@@ -3,7 +3,7 @@ sage.directive('presentValue', function() {
     templateUrl: "views/directives/presentValue.html",
     link: function($scope, attr, elem) {
 	  $scope.isUrl = function(value) {
-	    var pattern = RegExp('(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+@]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$', 'i');
+	    var pattern = new RegExp('(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+@]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$', 'i');
 	    return pattern.test(value);
 	  };
     }
