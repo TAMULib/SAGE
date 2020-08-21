5060a675a7e5c0ce999b29c117e86405375c117b
Jason Savell <jsavell@library.tamu.edu>
2020-08-21 09:15:32 -0500

    remove description field and validators

--- src/main/java/edu/tamu/sage/model/DiscoveryView.java
+++ src/main/java/edu/tamu/sage/model/DiscoveryView.java
@@ -62,10 +62,6 @@ public class DiscoveryView extends ValidatingBaseEntity {
     @Column(nullable = false)
     private String infoLinkUrl;
 
-    @NotNull
-    @Column(length = 1000, nullable = false)
-    private String description;
-
     private String queryParser;
 
     private String defaultOperand;
@@ -205,14 +201,6 @@ public class DiscoveryView extends ValidatingBaseEntity {
 
     public void setResultMetadataFields(List<MetadataField> resultMetadataFields) {
         this.resultMetadataFields = resultMetadataFields;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public MetadataField findMetadataFieldByKey(String key) {
