7ce60af1ac4748976a98e9e6965f09fc13f18a8e
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2021-08-31 15:30:12 -0500

    Using format field to determine player height

--- src/main/webapp/app/config/appConfig.js
+++ src/main/webapp/app/config/appConfig.js
@@ -56,6 +56,11 @@ var appConfig = {
     ]
   },
 
+  'audioVideoMap': {
+    "audio": "Sound Recording",
+    "video": "Moving Image"
+  },
+
   'defaultThumbnailURI': "resources/images/default-thumbnail.jpg",
   'defaultLoadingThumbnailURI': "resources/images/loading2.gif",
   'avalonUrl': 'avalon-pre.library.tamu.edu:443'
