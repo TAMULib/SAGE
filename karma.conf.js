152d5ef207f52920d86532ddb612c31a7e18d9fe
William Welling <8352733+wwelling@users.noreply.github.com>
2022-07-27 15:53:49 -0500

    correct karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -15,38 +15,41 @@ module.exports = function (config) {
 
       "src/main/webapp/app/config/apiMapping.js",
 
-      "src/main/webapp/app/node_modules/jquery/dist/jquery.js",
-      "src/main/webapp/app/node_modules/bootstrap/dist/js/bootstrap.js",
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/bootstrap/dist/js/bootstrap.js",
 
-      "src/main/webapp/app/node_modules/sockjs-client/dist/sockjs.js",
-      "src/main/webapp/app/node_modules/stompjs/lib/stomp.js",
+      "node_modules/sockjs-client/dist/sockjs.js",
+      "node_modules/stompjs/lib/stomp.js",
 
-      "src/main/webapp/app/node_modules/angular/angular.js",
+      "node_modules/angular/angular.js",
 
-      "src/main/webapp/app/node_modules/angular-sanitize/angular-sanitize.js",
-      "src/main/webapp/app/node_modules/angular-route/angular-route.js",
-      "src/main/webapp/app/node_modules/angular-loader/angular-loader.js",
-      "src/main/webapp/app/node_modules/angular-messages/angular-messages.js",
-      "src/main/webapp/app/node_modules/angular-mocks/angular-mocks.js",
+      "node_modules/angular-sanitize/angular-sanitize.js",
+      "node_modules/angular-route/angular-route.js",
+      "node_modules/angular-loader/angular-loader.js",
+      "node_modules/angular-messages/angular-messages.js",
+      "node_modules/angular-mocks/angular-mocks.js",
 
-      "src/main/webapp/app/node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js",
+      "node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js",
 
-      "src/main/webapp/app/node_modules/ng-csv/build/ng-csv.js",
+      "node_modules/ng-csv/build/ng-csv.js",
 
-      "src/main/webapp/app/node_modules/ng-table/bundles/ng-table.js",
+      "node_modules/ng-table/bundles/ng-table.js",
 
-      "src/main/webapp/app/node_modules/ng-file-upload/dist/ng-file-upload-shim.js",
-      "src/main/webapp/app/node_modules/ng-file-upload/dist/ng-file-upload.js",
+      "node_modules/ng-file-upload/dist/ng-file-upload-shim.js",
+      "node_modules/ng-file-upload/dist/ng-file-upload.js",
 
-      "src/main/webapp/app/node_modules/jasmine-promise-matchers/dist/jasmine-promise-matchers.js",
+      'node_modules/openseadragon/build/openseadragon/openseadragon.js',
+      'node_modules/ng-openseadragon/build/angular-openseadragon.js',
 
-      "src/main/webapp/app/node_modules/@wvr/core/app/config/coreConfig.js",
+      "node_modules/jasmine-promise-matchers/dist/jasmine-promise-matchers.js",
 
-      "src/main/webapp/app/node_modules/@wvr/core/app/components/**/*.js",
+      "node_modules/@wvr/core/app/config/coreConfig.js",
 
-      "src/main/webapp/app/node_modules/@wvr/core/app/core.js",
+      "node_modules/@wvr/core/app/components/**/*.js",
 
-      "src/main/webapp/app/node_modules/@wvr/core/app/**/*.js",
+      "node_modules/@wvr/core/app/core.js",
+
+      "node_modules/@wvr/core/app/**/*.js",
 
       "src/main/webapp/tests/testSetup.js",
 
